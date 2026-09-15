f5012b4b75ad91770150a8dd551386579912b59f
Wojciech <swirjestem@hotmail.com>
2026-09-15 09:55:30 +0200

    feat: initialize provider-backed browse UI

--- src/DragonDiskForge.App/App.xaml.cs
+++ src/DragonDiskForge.App/App.xaml.cs
@@ -10,7 +10,9 @@ public partial class App : Application
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        _window = new MainWindow();
+        var mainWindow = new MainWindow();
+        mainWindow.EnableDirectBrowseUi();
+        _window = mainWindow;
         _window.Activate();
     }
 }
